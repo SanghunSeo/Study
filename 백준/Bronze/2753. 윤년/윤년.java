8dd55e28ed580865d17a9248fc00dc38eb5b64aa
seoprize <sh65111595@gmail.com>
2023-11-13 13:56:20 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		if(T%4 == 0 && T%100 != 0) {
+			System.out.println(1);
+		}
+		else if(T%4 == 0 && T%400 == 0) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+		sc.close();
+	}
+
+}
