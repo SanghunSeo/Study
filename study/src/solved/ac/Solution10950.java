af0520597e2af5f0a493568e02c379dae83f54fd
서상훈 <shseo@inspire.partnes>
2023-11-20 09:45:50 +0900

    20231120

---
+++ study/src/solved/ac/Solution10950.java
@@ -0,0 +1,24 @@
+package solved.ac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution10950 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bf.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			sb.append(A + B + "\n");
+		}
+		System.out.println(sb);
+	}
+
+}
---
+++ study/src/solved/ac/Solution2741.java
@@ -0,0 +1,16 @@
+package solved.ac;
+
+import java.util.Scanner;
+
+public class Solution2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		
+		for(int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
